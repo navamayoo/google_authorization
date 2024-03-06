6932e52c4a4097c88893a272fc65b0b50305aec5
navamayoo <navamayoo@gmail.com>
2024-03-06 17:40:38 +0530

    get Google Credential

--- src/App.js
+++ src/App.js
@@ -1,29 +1,52 @@
+import "./App.css";
+import {
+  GoogleOAuthProvider,
+  GoogleLogin,
+  googleLogout,
+} from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
+import { useState } from "react";
 
-import './App.css';
-import { GoogleOAuthProvider, GoogleLogin  } from '@react-oauth/google';
+const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 function App() {
+  //const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+  const [user, setUser] = useState({});
 
-  const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+  console.log("CLIENT_ID>>>>", CLIENT_ID);
 
-  console.log("CLIENT_ID>>>>", CLIENT_ID)
+  const getGoogleCredential = (credential) => {
+    const userData = jwtDecode(credential);
+    setUser(userData);
+    console.log(userData);
+    //document.getElementById("signInDiv").hidden = true;
+  };
 
+  googleLogout();
   return (
     <div className="App">
       <div className="App-header">
-        <p>
-        React OAuth2 |  Auth0 Google Authentication
-        </p>
-        <GoogleOAuthProvider clientId={CLIENT_ID} >
-        <GoogleLogin
-  onSuccess={credentialResponse => {
-    console.log(credentialResponse);
-  }}
-  onError={() => {
-    console.log('Login Failed');
-  }}
-/>;
-          </GoogleOAuthProvider>;
+        <p>React OAuth2 | Auth0 Google Authentication</p>
+        {user && (
+          <>
+                    <div>
+            <img src={user.picture} alt={user.name} />
+            <h3>{user.name}</h3>
+          </div>
+          </>
+
+        )}
+
+        <GoogleOAuthProvider clientId={CLIENT_ID}>
+          <GoogleLogin
+            onSuccess={(credentialResponse) => {
+              getGoogleCredential(credentialResponse.credential);
+            }}
+            onError={() => {
+              googleLogout();
+            }}
+          />
+        </GoogleOAuthProvider>
       </div>
     </div>
   );
